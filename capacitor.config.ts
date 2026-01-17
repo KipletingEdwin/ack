80bedc8ca5ebdecd7830e158cb36fb7712420a42
Kipleting Edwin <kipletingedwin4@gmail.com>
2026-01-17 17:06:51 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ack',
+  webDir: 'www'
+};
+
+export default config;
