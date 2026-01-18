cd90d8a696de05a2d4a8c77091cc55312635b055
Kipleting Edwin <kipletingedwin4@gmail.com>
2026-01-18 16:44:59 +0000

    commit

--- ministries-grid/ministries-grid.component.ts
+++ ministries-grid/ministries-grid.component.ts
@@ -12,4 +12,12 @@ export class MinistriesGridComponent  implements OnInit {
 
   ngOnInit() {}
 
+  ministries = [
+    { name: 'Youth Ministry', icon: 'people', route: '/main/ministries' },
+    { name: 'Women’s Guild', icon: 'female', route: '/main/ministries' },
+    { name: 'Men’s Fellowship', icon: 'male', route: '/main/ministries' },
+    { name: 'Sunday School', icon: 'school', route: '/main/ministries' },
+    { name: 'Choir Ministry', icon: 'musical-notes', route: '/main/ministries' }
+  ];
+
 }
