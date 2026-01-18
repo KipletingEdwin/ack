a08c83f462f636f3caf6871fd8614269bf77b940
Kipleting Edwin <kipletingedwin4@gmail.com>
2026-01-18 09:59:42 +0000

    commit

--- footer/footer.component.ts
+++ footer/footer.component.ts
@@ -4,11 +4,14 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
+  standalone: false,
 })
 export class FooterComponent  implements OnInit {
 
   constructor() { }
 
   ngOnInit() {}
+
+  currentYear = new Date().getFullYear();
 
 }
--- navbar/navbar.component.ts
+++ navbar/navbar.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicModule } from "@ionic/angular";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  imports: [IonicModule],
+  standalone: false,
 })
 export class NavbarComponent  implements OnInit {
 
