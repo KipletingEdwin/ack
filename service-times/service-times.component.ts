55c84f0c4220556431d35a9ff78ab0670c71ad7f
Kipleting Edwin <kipletingedwin4@gmail.com>
2026-01-18 15:52:11 +0000

    add four components

---
+++ service-times/service-times.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-times',
+  templateUrl: './service-times.component.html',
+  styleUrls: ['./service-times.component.scss'],
+  standalone: false
+})
+export class ServiceTimesComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
